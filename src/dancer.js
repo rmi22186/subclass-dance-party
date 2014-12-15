a9ea4fdb817dea1ea757e3356688a5aec325c5f4
rmi22186 <rmi22186@gmail.com>
2014-12-15 15:32:51 -0800

    Convert functional pattern for dancer.js and blinkDancer.js to pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -1,33 +1,38 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
-
-  var dancer = {};
+var MakeDancer = function(top, left, timeBetweenSteps){
 
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');
 
-
-  dancer.step = function(){
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
-
-  dancer.setPosition = function(top, left){
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
-  };
-
+  this.step();
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
-
-  return dancer;
+  this.setPosition(top, left);
 };
+
+MakeDancer.prototype.step = function(timeBetweenSteps){
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+
+  // because setTimeout's 'this' is window, we must set the
+  // context of the instantiation to variable that, which
+  // is then bound to the new instantiation context
+  var that = this;
+  setTimeout(that.step.bind(that), timeBetweenSteps);
+};
+
+MakeDancer.prototype.setPosition = function(top, left){
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
+
+// test code
+/*var a = new MakeDancer(5, 5, 5);
+console.log(a);
+*/
