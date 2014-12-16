30ff698ae759064de761192852df52131626b832
rmi22186 <rmi22186@gmail.com>
2014-12-16 12:31:30 -0800

    Begin pairing feature, counter needs work

--- src/init.js
+++ src/init.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   window.dancers = [];
+  // used to determine the number of dancers currently on dance floor
+  var counter = 0;
+
   $('.lineUpButton').on('click', function(event){
     for (var i=0; i<window.dancers.length; i++) {
       window.dancers[i].setPosition($("body").height()-250);
@@ -8,30 +11,16 @@ $(document).ready(function(){
   /*$('span').mouseover(function() {
     $(this).animate({"left" : "-=50px"}, 500)
   });*/
-  $(".addDancerButton").on("click", function(event){
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on index.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
 
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
+  $(".addDancerButton").on("click", function(event){
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
     // make a dancer with a random position
-
     // calculates dancer y coordinate
     var y = ($("body").height()-250-250*Math.random());
     // calculates dancer x coordinate
     var x = Math.max($("body").width()*Math.random()-100,100);
-
 
     var dancer = new dancerMakerFunction(y, x, Math.random() * 1000
     );
@@ -40,9 +29,41 @@ $(document).ready(function(){
     dancer.y = y;
     dancer.x = x;
 
+    // add pair property to each dancer instance
+    dancer.pair = null;
+
     $('body').append(dancer.$node);
     window.dancers.push(dancer);
+    counter++;
+
+    // check if dancers > 1
+
+    if (counter -1 > 0) {
+      // check if previous dancer pair is null
+
+      if (window.dancers[0].pair === null) {
+        console.log(counter);
+        if (window.dancers[0].x === window.dancers[0].x) {
+          console.log(counter);
+          window.dancers[0].x = window.dancers[0].x - 50;
+          //console.log(window.dancers[counter]);
+        }
+       // new instance moves toward no pair instance (which is the dancer[i -1])
+         // determine coordiates of dancer[i -1] and dancer[i]
+           // if d2(x) is less than d1(x1)
+             // set d2(x) to d1(x + 10)
+           // else
+             // set reverse to true
+           // same if scenario for y
+       // change pair property to true for both [i and i -1]
+
+      }
+    }
+    // if every dancer is paired or no other dancers exist
+       // leave new dancer instance alone
+
+    //console.log(dancer);
+
 
   });
 });
-
