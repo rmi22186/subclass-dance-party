db4ff13114735d50b14742259d82ecb81ec8c876
rmi22186 <rmi22186@gmail.com>
2014-12-16 18:58:45 -0800

    Adds soundclound audio widget

--- src/init.js
+++ src/init.js
@@ -3,12 +3,30 @@ $(document).ready(function(){
   // used to determine the number of dancers currently on dance floor
   var counter = 0;
 
+
+  // on mouse hover each span animates
+  $('body').on('mouseover', 'span', function() {
+    var y = Math.random() * 100;
+    var x = Math.random() * 100;
+
+    var topArray = [y, -y];
+    var leftArray = [x, -x];
+
+    var topShift = topArray[Math.floor(Math.random() * 2)];
+    var leftShift = leftArray[Math.floor(Math.random() * 2)];
+
+    $(this).animate({top: '+='+topShift+'px', left: '+='+leftShift+'px'});
+  });
+
+  // sends all dancers to bottom of the page
   $('.lineUpButton').on('click', function(event){
     for (var i=0; i<window.dancers.length; i++) {
       window.dancers[i].setPosition($("body").height()-250);
     }
   });
 
+  // adds new dancer when dancer button is clicked
+  // brings every 2 dancers together as a pair
   $(".addDancerButton").on("click", function(event){
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
     // get the maker function for the kind of dancer we're supposed to make
